cffa067998f8ddc4e8155313f1a0e385a429cf4c
MagnetRise <nelsonfilipepaulosmoreira@hotmail.com>
2020-11-29 19:58:30 +0000

    move routes to views and change route with query

--- main.py
+++ main.py
@@ -7,31 +7,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
 
 db = SQLAlchemy(app)
 
-
-# Routes
-@app.route("/test")
-def test():
-    return render_template('dashindex.html', item_type_list=[{"name": "Test"}])
-
-@app.route("/")
-def index():
-    return render_template('dashindex.html', item_type_list=[{"name": "Test"}])
-
-@app.route("/item", methods=['GET'])
-def item_list():
-    objs = Item.query.all()
-    return jsonify([obj.__dict__ for obj in objs])
-
-
-@app.route("/item", methods=['POST'])
-def item_save():
-    new_item = db.Item(**request.form.__dict__)
-
-
-@app.route("/item_type")
-def index():
-    return render_template('index.html')
-
-
 if __name__ == '__main__':
+    from views import *
     app.run(debug=True)
---
+++ views.py
@@ -0,0 +1,32 @@
+from flask import render_template, jsonify, request
+
+from main import app, db
+from models import Item, ItemType
+
+
+@app.route("/test")
+def test():
+    item_type_list = [{"name": "Radio"}, {"name": "Baterias"}, {"name": "Chaves"}]
+    return render_template('dashindex.html', item_type_list=item_type_list)
+
+
+@app.route("/")
+def index():
+    item_type_list = [obj.__dict__ for obj in ItemType.query.all()]
+    return render_template('dashindex.html', item_type_list=item_type_list)
+
+
+@app.route("/item", methods=['GET'])
+def item_list():
+    objs = Item.query.all()
+    return jsonify([obj.__dict__ for obj in objs])
+
+
+@app.route("/item", methods=['POST'])
+def item_save():
+    new_item = db.Item(**request.form.__dict__)
+
+
+@app.route("/item_type")
+def item_type_list():
+    return render_template('dashindex.html')
